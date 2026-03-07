5c071625f330f915befdac07128fa38708c50739
jabar2025 <jabar09092002@gmaol.com>
2026-03-08 01:10:08 +0530

    Rough Commit

--- src/test/java/application/Audit.java
+++ src/test/java/application/Audit.java
@@ -4,4 +4,15 @@ public class Audit {
 
 	public UIMap.UIMapAudit uIMapAudit = new UIMap.UIMapAudit();
 	
+	
+	private void Asus() {
+		System.out.println("Asus");
+	}
+	private void Asus(int i) {
+		System.out.println("Asus");
+	}
+	private void Asus(String name) {
+		System.out.println("Asus");
+	}
+
 }
