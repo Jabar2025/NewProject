fb8383adac718a04f9b31106784fb96dec12cc40
jabar2025 <jabar09092002@gmaol.com>
2026-03-08 01:02:34 +0530

    Testing commit

--- src/test/java/application/Audit.java
+++ src/test/java/application/Audit.java
@@ -4,4 +4,11 @@ public class Audit {
 
 	public UIMap.UIMapAudit uIMapAudit = new UIMap.UIMapAudit();
 	
+	public void Audit() {
+	System.out.println("Audit");
+	}
+	
+	public void Audit(int i) {
+		System.out.println("Audit");
+	}
 }
